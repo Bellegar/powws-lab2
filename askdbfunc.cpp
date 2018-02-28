013b17a2c75d1b7d8860556d22c7e25bb69635f0
bellegar <zigteg@gmail.com>
2018-03-01 02:52:44 +0300

    separate askdb is work

---
+++ askdbfunc.cpp
@@ -0,0 +1,66 @@
+#ifndef ASKDB
+#define ASKDB
+#include <postgresql/libpq-fe.h>
+#include <iostream>
+#include <stdlib.h>
+#include <cstring>
+using namespace std;
+char* askdb(char* word, int tablenum)
+{
+		PGconn *conn;
+	    PGresult *res;
+		char out[300] ="";
+		int col, row;
+    	conn = PQconnectdb("dbname=bellegar host=localhost user=bellegar password=bugboom");
+    	if (conn == NULL)
+    	{
+    		strcpy(out, "no connection to bellegar DB\n");
+    		return out;
+    	}
+    	//std::cout << PQdb(conn);
+    	string namecol;
+    	switch (tablenum)
+    	{
+    	case 1:
+    		namecol = "name";
+    		break;
+    	case 2:
+    		namecol = "type";
+    		break;
+    	case 3:
+    		namecol = "book";
+    		break;
+    	}
+        //char* numch;
+        //sprintf(numch,"%d",tablenum);
+    	string comm = "select * from table";
+        comm += char(tablenum + (int)'0');
+        comm = comm + " where " + string(namecol) + " like '%" + string(word) + "%';";
+        //cout << tablenum<< ' '<< comm << "\n";
+    	res = PQexec(conn, comm.c_str());
+    	row = PQntuples(res);
+    	col = PQnfields(res);
+        //cout << row<<' ' << col <<"\n";
+   
+    if (row !=0)
+{
+ cout<<"table"<<tablenum<<":\n";
+	for (int icol = 0; icol < col; icol++)
+	{
+			strcat(out,PQfname(res,icol));
+			strcat(out,"\t");
+	}
+	strcat(out,"\n");
+}
+	for (int irow=0; irow<row; irow++)
+	{
+		for (int icol=0; icol < col; icol++)
+		{
+			strcat(out, PQgetvalue(res, irow, icol));
+			strcat(out,"\t");
+		}
+	strcat(out,"\n");
+	}
+cout << out;
+}
+#endif
--- test2db.cpp
+++ test2db.cpp
@@ -1,10 +1,13 @@
-#include <postgresql/libpq-fe.h>
-#include <iostream>
-#include <stdlib.h>
-#include <cstring>
+#ifndef TEST2DB
+#define TEST2DB
 #include <unistd.h>
 #include <sys/types.h>
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+//#include "askdbfunc.cpp"
 using namespace std;
+//namespace askdbfunc {char* askdb(char*,int);};
 char* askdb(char* word, int tablenum);
 int main(int argc, char** argv)
 {
@@ -18,7 +21,7 @@ int main(int argc, char** argv)
     		//int sockidW2 = socket(AF_UNIX, SOCK_STREAM, 0);
     		//connect(sockidW2, &files, (strlen(files.sa_data) +	sizeof(files.sa_family)));
     		//recv(sockidW2, (char*) buf, BUFSIZE,0);
-                cout << askdb("doll",2);
+                std::cout << askdb(argv[1],2);
     		//send(sockidW2, askdb(buf,2), BUFSIZE,0);
 
     	}
@@ -31,7 +34,7 @@ int main(int argc, char** argv)
         		//int sockidW3 = socket(AF_UNIX, SOCK_STREAM, 0);
         		//connect(sockidW3, &files,(strlen(files.sa_data) +	sizeof(files.sa_family)));
         		//recv(sockidW3, (char*) buf, BUFSIZE,0);
-                cout << askdb("doll",3);
+                std::cout << askdb(argv[1],3);
         		//send(sockidW3, askdb(buf,3), BUFSIZE,0);
     		}
     		else
@@ -41,68 +44,10 @@ int main(int argc, char** argv)
     	    	//int sockidW1 = socket(AF_UNIX, SOCK_STREAM, 0);
     	    	//connect(sockidW1, &files, (strlen(files.sa_data) +	sizeof(files.sa_family)));
     	    	//recv(sockidW1, (char*) buf, BUFSIZE,0);
-                cout << askdb("doll",1);
+                std::cout << askdb(argv[1],1);
         		//send(sockidW1, askdb(buf,1), BUFSIZE,0);
     		}
     	}
 }
 
-char* askdb(char* word, int tablenum)
-{
-		PGconn *conn;
-	    PGresult *res;
-		char out[300] ="";
-		int col, row;
-    	conn = PQconnectdb("dbname=bellegar host=localhost user=bellegar password=bugboom");
-    	if (conn == NULL)
-    	{
-    		strcpy(out, "no connection to bellegar DB\n");
-    		return out;
-    	}
-    	//std::cout << PQdb(conn);
-    	string namecol;
-    	switch (tablenum)
-    	{
-    	case 1:
-    		namecol = "name";
-    		break;
-    	case 2:
-    		namecol = "type";
-    		break;
-    	case 3:
-    		namecol = "book";
-    		break;
-    	}
-        //char* numch;
-        //sprintf(numch,"%d",tablenum);
-    	string comm = "select * from table";
-        comm += char(tablenum + (int)'0');
-        comm = comm + " where " + string(namecol) + " like '%" + string(word) + "%';";
-        //cout << tablenum<< ' '<< comm << "\n";
-    	res = PQexec(conn, comm.c_str());
-    	row = PQntuples(res);
-    	col = PQnfields(res);
-        //cout << row<<' ' << col <<"\n";
-   
-    if (row !=0)
-{
- cout<<"table"<<tablenum<<":\n";
-	for (int icol = 0; icol < col; icol++)
-	{
-			strcat(out,PQfname(res,icol));
-			strcat(out,"\t");
-	}
-	strcat(out,"\n");
-}
-	for (int irow=0; irow<row; irow++)
-	{
-		for (int icol=0; icol < col; icol++)
-		{
-			strcat(out, PQgetvalue(res, irow, icol));
-			strcat(out,"\t");
-		}
-	strcat(out,"\n");
-	}
-cout << out;
-}
-
+#endif
